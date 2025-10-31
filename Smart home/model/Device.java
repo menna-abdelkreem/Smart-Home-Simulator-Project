ac49659f9996310214d9414f31575ead7c8b83f3
Yara Emad <emadyara39@gmail.com>
2025-10-31 19:47:30 +0200

    Create abstract Device class with state management
    Added abstract Device class with properties and methods for managing device state and power consumption.

--- Smart home/model/Device.java
+++ Smart home/model/Device.java
@@ -1,1 +1,38 @@
+package model;
 
+public abstract class Device {
+    protected int id;
+    protected String name;
+    protected boolean status;
+    protected double powerConsumption;
+
+    public Device(int id, String name, double powerConsumption) {
+        this.id = id;
+        this.name = name;
+        this.powerConsumption = powerConsumption;
+        this.status = false;
+    }
+
+    public void turnOn() {
+        status = true;
+    }
+
+    public void turnOff() {
+        status = false;
+    }
+
+    public double getPowerUsage() {
+        return status ? powerConsumption : 0;
+    }
+
+    
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public boolean isOn() { return status; }
+    public double getPowerConsumption() { return powerConsumption; }
+
+    @Override
+    public String toString() {
+        return name + " [status=" + (status ? "ON" : "OFF") + "]";
+    }
+}
