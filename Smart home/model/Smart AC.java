fa07bec77e4eb0ec5398e1722c0bddcf01cf2446
Yara Emad <emadyara39@gmail.com>
2025-11-02 21:08:35 +0200

    Add AC class to manage temperature settings

--- Smart home/model/Smart AC.java
+++ Smart home/model/Smart AC.java
@@ -1,1 +1,18 @@
+package model;
 
+public class AC extends Device {
+    private int temperature;
+
+    public AC(int id, String name, double powerConsumption, int temperature) {
+        super(id, name, powerConsumption);
+        this.temperature = temperature;
+    }
+
+    public void setTemperature(int temp) {
+        this.temperature = temp;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+}
