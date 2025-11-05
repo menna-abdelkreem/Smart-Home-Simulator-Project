986324b121fcce8845634308994a057d447174f4
Yara Emad <emadyara39@gmail.com>
2025-11-05 09:44:39 +0200

    Add Door class with lock and unlock functionality

--- Smart home/model/Smart Door.java
+++ Smart home/model/Smart Door.java
@@ -1,1 +1,22 @@
+package model;
 
+public class Door extends Device {
+    private boolean isLocked;
+
+    public Door(int id, String name, double powerConsumption, boolean isLocked) {
+        super(id, name, powerConsumption);
+        this.isLocked = isLocked;
+    }
+
+    public void lock() {
+        isLocked = true;
+    }
+
+    public void unlock() {
+        isLocked = false;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+}
