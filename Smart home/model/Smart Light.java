e8a441e796fb1a6e7d999adbb21ded7b2ff31747
Yara Emad <emadyara39@gmail.com>
2025-11-07 18:42:02 +0200

    Update Smart Light.java

--- Smart home/model/Smart Light.java
+++ Smart home/model/Smart Light.java
@@ -1,1 +1,20 @@
 
+package model;
+
+public class Light extends Device {
+    private int brightness;
+
+    public Light(int id, String name, double powerConsumption, int brightness) {
+        super(id, name, powerConsumption);
+        this.brightness = brightness;
+    }
+
+    public void setBrightness(int level) {
+        if (level >= 0 && level <= 100)
+            this.brightness = level;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+}
