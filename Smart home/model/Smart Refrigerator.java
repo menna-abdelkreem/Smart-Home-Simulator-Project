23933e580856191a2d5e4d39a1233de4ba1171c3
Yara Emad <emadyara39@gmail.com>
2025-11-21 09:44:23 +0200

    Add Refrigerator class with temperature management

--- Smart home/model/Smart Refrigerator.java
+++ Smart home/model/Smart Refrigerator.java
@@ -1,1 +1,18 @@
+package model;
 
+public class Refrigerator extends Device {
+    private int internalTemp;
+
+    public Refrigerator(int id, String name, double powerConsumption, int internalTemp) {
+        super(id, name, powerConsumption);
+        this.internalTemp = internalTemp;
+    }
+
+    public void setInternalTemp(int temp) {
+        this.internalTemp = temp;
+    }
+
+    public int getInternalTemp() {
+        return internalTemp;
+    }
+}
